4a9ff2f8d358723d5476bf08c76efb9db616dadf
choks <choks0225@naver.com>
2022-02-04 02:07:24 +0900

    refactor: styled-components 추가 및 Modal 임시 작업

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,29 @@
+import { CloseModalButton, CreateModal } from '@components/Modal/styled';
+import React, { FC, ReactNode, useCallback } from 'react';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+}
+
+//
+const Modal = ({ show, children, onCloseModal }: { show: boolean; children: ReactNode; onCloseModal: () => void }) => {
+  // const Modal: FC<Props> = ({ show, children, onCloseModal }) => {
+  const stopPropagation = useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+
+  if (!show) {
+    return null;
+  }
+  return (
+    <CreateModal onClick={onCloseModal}>
+      <div onClick={stopPropagation}>
+        <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>
+        {children}
+      </div>
+    </CreateModal>
+  );
+};
+
+export default Modal;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
+import Modal from '@components/Modal';
 import '@styles/globals.scss';
 import { Provider } from 'mobx-react';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+
+      {/*  TODO: Modal -> store로 관리할수있게 모듈화 처리 하면될듯*/}
+      <Modal show={false} onCloseModal={() => {}}>
+        <form>
+          <div>sadasd</div>
+        </form>
+      </Modal>
+    </>
+  );
 }
 
 export default MyApp;
